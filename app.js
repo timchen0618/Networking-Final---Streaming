104b9d48f67121eb80870dff972caa0aeca83fea
Tim Chen (Hung-Ting, Chen) <35536646+timchen0618@users.noreply.github.com>
2019-06-06 15:38:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+,	path = require('path')
+,	streams = require('./app/streams.js')();
+
+var favicon = require('serve-favicon')
+,	logger = require('morgan')
+,	methodOverride = require('method-override')
+,	bodyParser = require('body-parser')
+,	errorHandler = require('errorhandler');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.set('ip', process.env.SERVER_IP || "127.0.0.1")
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+
+
+// routing
+require('./app/routes.js')(app, streams);
+
+//var server = app.listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
+var server = app.listen(app.get('port'), "10.131.185.68" );
+var io = require('socket.io').listen(server);
+/**
+ * Socket.io event handling
+ */
+require('./app/socketHandler.js')(io, streams);
+
+//10.131.185.68
